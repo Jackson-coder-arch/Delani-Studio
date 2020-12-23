ee69c5a09d00c30715859958f419a6797da2e0e9
Jackson-coder-arch <jacksonikonya@gmail.com>
2020-12-23 11:43:09 +0300

    addition of show and hide

--- js/script.js
+++ js/script.js
@@ -21,47 +21,46 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){
-    $("#work-4").mouseover(function(){
-        $("#text4").show();
+    $(".work-4").mouseover(function(){
+        $("#text4-overlay").show();
     }).mouseout(function(){
-        $("#text4").hide();
+        $("#text4-overlay").hide();
     });
-    $("#work-3").mouseover(function(){
-        $("#text3").show();
+    $(".work-3").mouseover(function(){
+        $("#text3-overlay").show();
     }).mouseout(function(){
-        $("#text3").hide();
+        $("#text3-overlay").hide();
     });
-    $("#work-2").mouseover(function(){
-        $("#text2").show();
+    $(".work-2").mouseover(function(){
+        $("#text2-overlay").show();
     }).mouseout(function(){
-        $("#text2").hide();
+        $("#text2-overlay").hide();
     });    
-    });$("#work-1").mouseover(function(){
-        $("#text1").show();
+    });$(".work-1").mouseover(function(){
+        $("#text1-overlay").show();
     }).mouseout(function(){
-        $("#text1").hide();
+        $("#text1-overlay").hide();
     });
-    $("#work-5").mouseover(function(){
-        $("#text5").show();
+    $(".work-5").mouseover(function(){
+        $("#text5-overlay").show();
     }).mouseout(function(){
-        $("#text5").hide();
+        $("#text5-overlay").hide();
     });    
-    ;$("#work-6").mouseover(function(){
-        $("#text6").show();
+    ;$(".work-6").mouseover(function(){
+        $("#text6-overlay").show();
     }).mouseout(function(){
-        $("#text6").hide();
+        $("#text6-overlay").hide();
     });    
-    ;$("#work-7").mouseover(function(){
-        $("#text7").show();
+    ;$(".work-7").mouseover(function(){
+        $("#text7-overlay").show();
     }).mouseout(function(){
-        $("#text7").hide();
+        $("#text7-overlay").hide();
     });    
-    $("#work-8").mouseover(function(){
-        $("#text8").show();
+    $(".work-8").mouseover(function(){
+        $("#text8-overlay").show();
     }).mouseout(function(){
-        $("#text8").hide();
+        $("#text8-overlay").hide();
     });
-
 
 
 $("form#form1").on('submit',function(event){
@@ -74,11 +73,11 @@ $("form#form1").on('submit',function(event){
     if ($("input#name").val() && $("input#email").val() && $("input#message").val() ){
         alert ("Hello" +name + ", Thank you for reaching out, we'll get back to you shortly..");
     }
-    else if($("input#name").val() && $("input#email").val()){
+    else if($("input#name").val() || $("input#email").val()){
         alert ("Thank you for reaching out to us. If you wish to leave a message please type in on the message section..");
     }
     else{
-        alert ("Please provide correct name or email")
+        alert ("Please provide correct name or email");
     }
 
 });
